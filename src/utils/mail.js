96203050e275d4792c97785559a5700b2902f488
= <mrsohel.biswas@gmail.com>
2025-12-03 11:57:35 +0530

    updated code with Mailgen

--- src/utils/mail.js
+++ src/utils/mail.js
@@ -1,5 +1,46 @@
 import { text } from "express";
 import Mailgen from "mailgen";
+import nodemailer from "nodemailer";
+
+const sendMail = async (options) => {
+  const mailGenertor = new Mailgen({
+    theme: "default",
+    product: {
+      name: "Task Manager",
+      link: "https://task-manager.com",
+      logo: "task-manager.com",
+    },
+  });
+
+  const mailTextual = mailGenertor.generatePlaintext(options.MailgenContent);
+  const mailHTML = mailGenertor.generate(options.MailgenContent);
+
+  const transporter = nodemailer.createTransport({
+    host: process.env.MAILTRAL_SMTP_HOST,
+    port: process.env.MAILTRAL_SMTP_PORT,
+    auth: {
+      user: process.env.MAILTRAL_SMTP_USERNAMe,
+      pass: process.env.MAILTRAL_SMTP_PASSWORD,
+    },
+  });
+
+  const mail = {
+    from: "mail.taskmanager.com",
+    to: options.email,
+    subject: options.subject,
+    text: mailTextual,
+    html: mailHTML,
+  };
+
+  try {
+    await transporter.sendMail(mail);
+  } catch (error) {
+    console.error(
+      "Email service failed. Make sure you have provided the Mailtrap creds in the .env file",
+    );
+    console.error("Error:", error);
+  }
+};
 
 const emailVerificationMailgenContent = (username, verificationUrl) => {
   return {
@@ -39,4 +80,8 @@ const forgotPasswordMailgenContent = (username, passwordResetUrl) => {
   };
 };
 
-export { emailVerificationMailgenContent, forgotPasswordMailgenContent };
+export {
+  emailVerificationMailgenContent,
+  forgotPasswordMailgenContent,
+  sendMail,
+};
